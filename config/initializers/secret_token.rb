0106b42207b81cf211648430992278d9e1d565cf
kende <kende.j@gmail.com>
2011-07-14 17:44:01 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Startrockit::Application.config.secret_token = 'd5686c6c22c1d80e00f2d507118ce48f21cf9068e35601ca7346a1f2b23e3d8f00b0eb5441003d4f07a3eb3bff9e1aff7b7e6b29e0acfb5d3eb97631e39a94e9'
